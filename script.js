26fc25f17f8be09efe0af3ae5d419d3179f43631
Souvlaki42 <souvlaki420@gmail.com>
2022-09-24 13:25:15 +0300

    Maintenance 3

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const isDown = true;
-if (isDown) window.location.href = "down";
+const maintenanceMode = true;
+let urlPrefix = "";
+if (window.location.hostname !== "souvlaki42.github.io") {
+    urlPrefix = ".html";
+}
+if (maintenanceMode) {
+    window.location.href = `down${urlPrefix}`;
+}
