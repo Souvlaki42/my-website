07284fd34fa1487fe8ced74256573c4b8153930c
Souvlaki42 <souvlaki420@gmail.com>
2022-09-24 13:51:38 +0300

    Configuarations

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const maintenanceMode = true;
-let urlPrefix = "";
-if (window.location.hostname !== "souvlaki42.github.io") {
-    urlPrefix = ".html";
+execute = async () => {
+	const response = await fetch("https://api.npoint.io/5c3b59ca8257e0ee5272").then((res) => res.json()).catch((err) => console.log(err));
+	const maintenanceMode = response.souvlaki42.maintenanceMode;
+	let urlPrefix = "";
+	if (window.location.hostname !== "souvlaki42.github.io") {
+		urlPrefix = ".html";
+	}
+	if (maintenanceMode) {
+		window.location.href = `down${urlPrefix}`;
+	}
 }
-if (maintenanceMode) {
-    window.location.href = `down${urlPrefix}`;
-}
+
+execute();
