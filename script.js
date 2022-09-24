cf77ae5ce2ee90e7e6cd1203ec0a72059c97f9f5
Souvlaki42 <souvlaki420@gmail.com>
2022-09-24 15:37:07 +0300

    Bug Fixing 3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let urlPrefix = "";
+let domainUrl = `${window.location.protocol}//${window.location.hostname}${window.location.port === "80" || window.location.port === "443" ? "" : ":" + window.location.port}`;
 const backBtn = document.querySelector("[data-back-btn]");
-if (backBtn) backBtn.href = `${window.location.protocol}//${window.location.hostname}:${window.location.port === "80" || window.location.port === "443" ? "" : window.location.port}`;
+if (backBtn) backBtn.href = domainUrl;
 execute = async () => {
 		const response = await fetch("https://api.npoint.io/5c3b59ca8257e0ee5272").then((res) => res.json()).catch((err) => console.log(err));
 		const maintenanceMode = response.souvlaki42.maintenanceMode;
@@ -8,10 +9,6 @@ execute = async () => {
 		if (window.location.hostname !== "souvlaki42.github.io") {
 			urlPrefix = ".html";
 		}
-		if (maintenanceMode) {
-			if (window.location.href !== `${window.location.protocol}//${window.location.hostname}:${window.location.port === "80" || window.location.port === "443" ? "" : window.location.port}/down${urlPrefix}`) {
-			window.location.href = `down${urlPrefix}`;
-		}
+		if (maintenanceMode && window.location.href !== `${domainUrl}/down${urlPrefix}`) window.location.href = `down${urlPrefix}`;
 	}
-}
 execute();
