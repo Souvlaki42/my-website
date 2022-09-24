90458b35cf8d017815e8b38604170f21da828377
Souvlaki42 <souvlaki420@gmail.com>
2022-09-24 16:00:20 +0300

    Bug Fixing 4

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let urlPrefix = "";
-let domainUrl = `${window.location.protocol}//${window.location.hostname}${window.location.port === "80" || window.location.port === "443" ? "" : ":" + window.location.port}`;
+let portCheck = window.location.port === "80" || window.location.port === "443" || window.location.port === "" || window.location.port === undefined || window.location.port === null ? "" : ":" + window.location.port
+const domainUrl = `${window.location.protocol}//${window.location.hostname}${portCheck}`;
 const backBtn = document.querySelector("[data-back-btn]");
 if (backBtn) backBtn.href = domainUrl;
-execute = async () => {
+
+async function execute() {
 		const response = await fetch("https://api.npoint.io/5c3b59ca8257e0ee5272").then((res) => res.json()).catch((err) => console.log(err));
 		const maintenanceMode = response.souvlaki42.maintenanceMode;
 
 		if (window.location.hostname !== "souvlaki42.github.io") {
 			urlPrefix = ".html";
 		}
+		console.log(`${domainUrl}/down${urlPrefix}`);
 		if (maintenanceMode && window.location.href !== `${domainUrl}/down${urlPrefix}`) window.location.href = `down${urlPrefix}`;
 	}
 execute();
