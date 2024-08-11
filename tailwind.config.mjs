b22c6d3ece9bb99189907d6caa2aea210465a43e
Souvlaki42 <souvlaki420@gmail.com>
2024-08-12 02:46:32 +0300

    Homepage done for now

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,13 @@
-import type { Config } from "tailwindcss";
-
-const config = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        mocha: "#1e1e2e",
+        "sky-blue": "#6CB4FF",
+        "light-blue": "#94D0FF",
+      },
       keyframes: {
         wave: {
           "0%": { "background-position-x": "1000px" },
@@ -17,6 +21,4 @@ const config = {
     },
   },
   plugins: [],
-} satisfies Config;
-
-export default config;
+};
