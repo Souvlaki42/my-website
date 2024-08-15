9368a29ef485b8ab0a4288322b6fd578fe0eac7f
Souvlaki42 <souvlaki420@gmail.com>
2024-08-15 18:16:28 +0300

    Remove .vercel

--- .vercel/output/functions/_render.func/home/ilias/projects/my-website/.vercel/output/_functions/chunks/my-first-post_DpPnfeuY.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./my-first-post_BagQUffX.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- astro.config.mjs
+++ astro.config.mjs
@@ -6,29 +6,27 @@ import { AppConfig } from "./src/lib/utils";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   site: AppConfig.site,
   prefetch: {
-    prefetchAll: true,
+    prefetchAll: true
   },
   experimental: {
-    actions: true,
+    actions: true
   },
-  integrations: [
-    tailwind(),
-    AutoImport({
-      imports: ["./src/components/Code.astro"],
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), AutoImport({
+    imports: ["./src/components/Code.astro"]
+  }), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "catppuccin-mocha",
-      wrap: true,
-    },
+      wrap: true
+    }
   },
-  devToolbar: { enabled: false },
+  devToolbar: {
+    enabled: false
+  }
 });
