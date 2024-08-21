1d2f809208d35cbbdf30bc19147ab04c5debc60d
Souvlaki42 <souvlaki420@gmail.com>
2024-08-21 18:17:27 +0300

    Fixed robots.txt

--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,7 @@ export const AppConfig = {
   image: "opengraph.jpg", // this will be used as the default social preview image
   twitter: "@souvlaki42",
   site:
-    import.meta.env.MODE === "production"
+    process.env.NODE_ENV === "production"
       ? "https://souvlaki.me/"
       : "http://localhost:4321" // this is your website URL
 };
